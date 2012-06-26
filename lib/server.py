5d4e7578dc73a23bb60fb5561db1eb26e1d52dc5
Ryon Sherman <ryon.sherman@redcheetah.com>
2012-06-26 16:09:39 -0500

    Updated route handling

--- lib/server.py
+++ lib/server.py
@@ -7,16 +7,17 @@ class Server(object):
     class Handler(SimpleHTTPRequestHandler):
         _routes = {}
 
-        def _return_auth(self):
+        def _response_auth(self):
             self.send_response(401)
             self.send_header("WWW-Authenticate", "Basic realm=\"uTorrent Bridge\"")
             self.send_header("Content-type", "text/html")
             self.end_headers()
 
-        def _return_success(self):
+        def _response_default(self, response=None):
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
+            self.wfile.write(response)
 
         def _auth_basic(self):
             header = self.headers.getheader('Authorization')
@@ -31,11 +32,17 @@ class Server(object):
             return False
 
         def do_GET(self):
-            auth = getattr(self, "_auth_%s" % self.auth)
-            self._return_success() if auth() else self._return_auth()
+            path = self.path.split('?')
+            route = self._routes.get(path[0], self._routes['/'])
 
-            route = self._routes.get(self.path)
-            self.wfile.write(route() if route else self._routes['/']())
+            args = dict([arg.split('=') for arg in path[1].split('&')]) if len(path) > 1 else {}
+
+            if route['auth']:
+                auth = getattr(self, "_auth_%s" % self.auth)
+                if not auth():
+                    return self._response_auth()
+
+            self._response_default(route['callback'](**args))
 
     def __init__(self, *args, **kwargs):
         self.address = kwargs['address']
@@ -48,7 +55,7 @@ class Server(object):
 
         self.server = HTTPServer((self.address, self.port), self.handler)
 
-        self.routes['/'] = self.route_default
+        self.add_route('/', self.response_default)
 
     @property
     def routes(self):
@@ -60,8 +67,8 @@ class Server(object):
     def stop(self):
         self.server.server_close()
 
-    def add_route(self, route, callback):
-        self.server.RequestHandlerClass._routes[route] = callback
+    def add_route(self, path, callback, auth=False):
+        self.routes[path] = {'callback': callback, 'auth': auth}
 
-    def route_default(self):
+    def response_default(self, *args, **kwargs):
         return "<html></html>"
