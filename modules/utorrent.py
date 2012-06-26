a491b8cf17ba934b6eae14d201dae60787bc4a27
Ryon Sherman <ryon.sherman@gmail.com>
2012-06-25 19:57:16 -0500

    Created uTorrent and rTorrent module boilerplates.

---
+++ modules/utorrent.py
@@ -0,0 +1,14 @@
+from lib.client import Client
+from lib.server import Server
+
+
+class Client(Client):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class Server(Server):
+
+    def __init__(self, *args, **kwargs):
+        pass
