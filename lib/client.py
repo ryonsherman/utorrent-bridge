4d12e6d03556a7a8d76527f4763760774cdf91fb
Ryon Sherman <ryon.sherman@gmail.com>
2012-06-27 03:09:26 -0500

    Provided basic http auth and request functionality.

--- lib/client.py
+++ lib/client.py
@@ -1,4 +1,28 @@
 class Client(object):
 
+    def _request_http(self, string):
+        from urllib2 import Request, urlopen
+
+        url = "http://%s:%s/%s" % (self.address, self.port, string.lstrip('/'))
+        self._request = Request(url)
+
+        auth = getattr(self, "_auth_%s" % self.auth)
+        if auth:
+            auth()
+
+        response = urlopen(self._request, None, 3)
+        return response
+
+    def _auth_basic(self):
+        from base64 import encodestring
+
+        header = "Basic %s" % encodestring("%s:%s" % (self.username, self.password))[:-1]
+        self._request.add_header("Authorization", header)
+
     def __init__(self, *args, **kwargs):
-        pass
+        self.address = kwargs['address']
+        self.port = kwargs['port']
+
+        self.auth = kwargs.get('auth')
+        self.username = kwargs.get('username')
+        self.password = kwargs.get('password')
