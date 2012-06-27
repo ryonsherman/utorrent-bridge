312346a742d328702bebfc9c9928d421497e8778
Ryon Sherman <ryon.sherman@redcheetah.com>
2012-06-27 14:14:52 -0500

    Provided basic uTorrent Server and rTorrent Client action methods.

--- modules/rtorrent.py
+++ modules/rtorrent.py
@@ -2,81 +2,119 @@ from lib.client import Client
 from lib.server import Server
 
 
+class rTorrent:
+
+    BUILD = '0.9.2'
+
+    # def get_transfers()
+
+    # def action_add_url()
+    # def action_start()
+    # def action_stop()
+    # def action_pause()
+    # def action_unpause()
+    # def action_recheck()
+    # def action_remove()
+    # def action_restart()
+
+
 class Client(Client):
 
-    class RPC(object):
+    class RPC:
 
-        def __init__(self):
-            pass
+        def __init__(self, address, port):
+            from xmlrpclib import Server
 
-        def call(self):
-            pass
+            self._rpc = Server("http://%s:%s/RPC2" % (address, port))
+
+        def call(self, method, params, multicall=True):
+            if type(params) == list and multicall:
+                return self.multicall(method, params)
+            else:
+                return getattr(self._rpc, method)(params)
+
+        def multicall(self, method, params):
+            calls = []
+            for param in params:
+                calls.append({'methodName': method, 'params': [param]})
+            return self._multicall(calls)
+
+        def _multicall(self, calls):
+            return self._rpc.system.multicall(calls)
+
+    _methods = {
+        'd.get_state': ['status'],
+        'd.get_name': ['name'],
+        'd.get_size_bytes': ['size'],
+        'd.get_completed_bytes': ['downloaded'],
+        'd.get_up_total': ['uploaded'],
+        'd.get_ratio': ['ratio'],
+        'd.get_up_rate': ['upload_speed'],
+        'd.get_down_rate': ['download_speed'],
+        'd.get_peers_accounted': ['peers_connected'],
+        'd.get_peers_complete': ['seeds_connected'],
+        'd.get_left_bytes': ['remaining']
+    }
 
     def __init__(self, *args, **kwargs):
         super(Client, self).__init__(*args, **kwargs)
 
-        from xmlrpclib import Server
-        url = "http://%s:%s/RPC2" % (self.address, self.port)
-        self._rpc = Server(url)
+        self._rpc = self.RPC(self.address, self.port)
 
     def get_transfers(self, views=['main']):
-        hashes = self._rpc.download_list(views)
+        hashes = self._rpc.call('download_list', views, False)
 
         calls = []
+        fields = {}
         for hash in hashes:
-            calls.append({'methodName': 'd.get_tracker_size', 'params': [hash]})
-        # tracker_sizes = self._rpc.system.multicall(calls)
+            fields[hash] = []
+            for field in dir(self.server.Transfer):
+                for method in self._methods.keys():
+                    if field in self._methods[method]:
+                        calls.append({'methodName': method, 'params': [hash]})
+                        fields[hash].append(field)
 
-        calls = []
-        for index, hash in enumerate(hashes):
-            for method in [
-                'd.is_active',
-                'd.get_name',
-                'd.get_size_bytes',
-                'd.get_completed_bytes',
-                'd.get_up_total',
-                'd.get_ratio',
-                'd.get_up_rate',
-                'd.get_down_rate',
-                'd.get_peers_accounted',
-                'd.get_peers_complete',
-                'd.get_left_bytes'
-                ]:
-                calls.append({'methodName': method, 'params': [hash]})
+        transfers = []
+        if calls:
+            values = self._rpc._multicall(calls)
 
-        details = self._rpc.system.multicall(calls)
+            index = 0
+            for hash in hashes:
+                transfer = self.server.Transfer()
+                transfer.hash = hash
 
-        cursor = 0
-        transfers = {}
-        for hash in hashes:
-            transfer = {}
-            transfer['hash'] = hash
+                for field in fields[hash]:
+                    setattr(transfer, field, values[index][0])
+                    index += 1
 
-            for detail in [
-                'status',
-                'name',
-                'size',
-                'downloaded',
-                'uploaded',
-                'ratio',
-                'upload_speed',
-                'download_speed',
-                'peers_connected',
-                'seeds_connected',
-                'remaining'
-                ]:
-                transfer[detail] = details[cursor][0]
-                cursor += 1
-
-            transfers[hash] = transfer
+                transfers.append(transfer)
 
         return transfers
 
-    def action_start(self, hash):
-        self._rpc.d.start(hash)
+    def action_start(self, hashes):
+        self._rpc.call('d.start', hashes)
 
-    def action_stop(self, hash):
-        self._rpc.d.stop(hash)
+    def action_stop(self, hashes):
+        self._rpc.call('d.close', hashes)
+
+    def action_pause(self, hashes):
+        self._rpc.call('d.stop', hashes)
+
+    def action_unpause(self, hashes):
+        self._rpc.call('d.resume', hashes)
+
+    def action_recheck(self, hashes):
+        self._rpc.call('d.check_hash', hashes)
+
+    def action_remove(self, hashes):
+        self._rpc.call('d.erase', hashes)
+
+    def action_restart(self, hashes):
+        self.action_stop(hashes)
+        self.action_start(hashes)
+
+    def action_add_url(self, urls):
+        self._rpc.call('load', urls)
 
 
 class Server(Server):
--- modules/utorrent.py
+++ modules/utorrent.py
@@ -1,8 +1,10 @@
+from __future__ import division
+
 from lib.client import Client
 from lib.server import Server
 
 
-class uTorrent(object):
+class uTorrent:
 
     BUILD = 27220
 
@@ -32,12 +34,35 @@ class uTorrent(object):
         NORMAL = 2
         HIGH = 3
 
+    # def get_token()
     # def get_transfers()
+
+    # def action_add_file()
+    # def action_add_url()
+    # def action_start()
+    # def action_stop()
+    # def action_forcestart()
+    # def action_pause()
+    # def action_unpause()
+    # def action_restart()
+    # def action_getfiles()
+    # def action_getprops()
+    # def action_getsettings()
+    # def action_setsetting()
+    # def action_recheck()
+    # def action_removedata
+    # def action_setprio()
+    # def action_setprops()
 
 
 class Client(Client):
 
     _token = None
+
+    def _request_http(self, string):
+        string = "%s&token=%s" % (string, self._token)
+
+        return super(Client, self)._request_http(string)
 
     def __init__(self, *args, **kwargs):
         super(Client, self).__init__(*args, **kwargs)
@@ -53,15 +78,35 @@ class Client(Client):
             response.close()
 
             token = dom.getElementsByTagName('div')[0].childNodes[0].data
+
             return token
-
-    def _request_http(self, string):
-        string = "%s&token=%s" % (string, self._token)
-
-        return super(Client, self)._request_http(string)
 
 
 class Server(Server):
+
+    class Transfer:
+        hash = ''
+        status = 0
+        name = ''
+        size = 0
+        downloaded = 0
+        uploaded = 0
+        ratio = 0
+        upload_speed = 0
+        download_speed = 0
+        eta = 0
+        label = ''
+        peers_connected = 0
+        peers_in_swarm = 0
+        seeds_connected = 0
+        seeds_in_swarm = 0
+        availability = 0
+        torrent_queue_order = 0
+        remaining = 0
+
+        @property
+        def percent_progress(self):
+            return int(self.downloaded / self.size * 1000)
 
     _token_cache = []
     _response = {'build': uTorrent.BUILD}
@@ -69,8 +114,8 @@ class Server(Server):
     def __init__(self, *args, **kwargs):
         super(Server, self).__init__(*args, **kwargs)
 
-        self.add_route('/gui/', self.response_gui, True)
-        self.add_route('/gui/token.html', self.response_token_html, True)
+        self._add_route('/gui/', self.route_gui, True)
+        self._add_route('/gui/token.html', self.route_token_html, True)
 
     def _write_cache(self):
         pass
@@ -78,23 +123,22 @@ class Server(Server):
     def _read_cache(self, cache_id):
         pass
 
-    def response_default(self, *args, **kwargs):
+    def route_default(self, *args, **kwargs):
         return "\ninvalid request"
 
-    def response_token_html(self, *args, **kwargs):
+    def route_token_html(self, *args, **kwargs):
         return "<html><div id='token' style='display:none;'>%s</div></html>" % self.get_token()
 
-    def response_gui(self, *args, **kwargs):
-        # TODO: Enable this after dev.
-        # if not kwargs.get('token') or kwargs['token'] not in self._token_cache:
-        #     return self.response_default()
+    def route_gui(self, *args, **kwargs):
+        if kwargs.get('token') and kwargs['token'] not in self._token_cache:
+            return self.route_default()
 
         if kwargs.get('list'):
             self.get_transfers(cache_id=kwargs.get('cid'))
 
         action = kwargs.get('action')
         if action:
-            method = 'action_%s' % action
+            method = 'action_%s' % action.replace('-', '_')
             if hasattr(self, method):
                 action = getattr(self, method)
                 action(**kwargs)
@@ -117,7 +161,7 @@ class Server(Server):
         })
 
         transfers = self.client.get_transfers()
-        for transfer in transfers.values():
+        for transfer in transfers:
             torrent = []
             for detail in [
                 'hash',
@@ -140,7 +184,7 @@ class Server(Server):
                 'torrent_queue_order',
                 'remaining'
                 ]:
-                torrent.append(transfer.get(detail, 0))
+                torrent.append(getattr(transfer, detail))
             self._response['torrents'].append(torrent)
 
     def action_start(self, *args, **kwargs):
@@ -148,3 +192,28 @@ class Server(Server):
 
     def action_stop(self, *args, **kwargs):
         self.client.action_stop(kwargs.get('hash'))
+
+    def action_pause(self, *args, **kwargs):
+        getattr(self.client, 'action_pause', self.client.action_stop)(kwargs.get('hash'))
+
+    def action_unpause(self, *args, **kwargs):
+        getattr(self.client, 'action_unpause', self.client.action_start)(kwargs.get('hash'))
+
+    def action_recheck(self, *args, **kwargs):
+        self.client.action_recheck(kwargs.get('hash'))
+
+    def action_remove(self, *args, **kwargs):
+        self.client.action_remove(kwargs.get('hash'))
+
+    def action_removedata(self, *args, **kwargs):
+        getattr(self.client, 'action_removedata', self.client.remove)(kwargs.get('hash'))
+
+    def action_restart(self, *args, **kwargs):
+        if not getattr(self.client, 'action_restart'):
+            self.action_stop(*args, **kwargs)
+            self.action_start(*args, **kwargs)
+        else:
+            self.client.action_restart(kwargs.get('hash'))
+
+    def action_add_url(self, *args, **kwargs):
+        self.client.action_add_url(kwargs.get('s'))
